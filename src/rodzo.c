dc63a819c2009c9bffb7a75709bd752b77bb792e
John Mettraux <jmettraux@gmail.com>
2013-12-11 07:55:57 +0900

    use -v Valgrind switch for rvspec
    and fix one tiny error

--- src/rodzo.c
+++ src/rodzo.c
@@ -489,7 +489,7 @@ void add_spec_files(int *count, char **names, char *path)
   struct dirent *ep;
   while ((ep = readdir(dir)) != NULL)
   {
-    char *fn = malloc((strlen(path) + strlen(ep->d_name) + 2) * sizeof(char));
+    char *fn = malloc((strlen(path) + strlen(ep->d_name) + 4) * sizeof(char));
     sprintf(fn, "%s/%s", path, ep->d_name);
 
     add_spec_file(count, names, fn);
