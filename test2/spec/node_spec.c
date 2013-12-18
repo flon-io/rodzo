c936169f628d542ed513876f9d30144ec4426051
John Mettraux <jmettraux@gmail.com>
2013-12-19 07:47:22 +0900

    prepare aabro bug hunt

---
+++ test2/spec/node_spec.c
@@ -0,0 +1,30 @@
+
+//
+// specifying aabro
+//
+// Wed Dec 18 16:19:13 JST 2013
+//
+
+#include "aabro.h"
+
+
+describe "abr_node"
+{
+  describe "abr_malloc_node()"
+  {
+    it "creates a node"
+    {
+      ensure(1 == 2);
+    }
+  }
+  describe "abr_node_to_string(n)"
+  {
+    it "returns a string representation of the node"
+    {
+      //abr_node *n = abr_malloc_node("nada", 0);
+      //ensure(strcmp("[ nada, 0 ]", abr_node_to_string(n)) == 0);
+      ensure(4 == 5);
+    }
+  }
+}
+
---
+++ test2/spec/parse_string_spec.c
@@ -0,0 +1,19 @@
+
+//
+// specifying aabro
+//
+// Tue Dec 17 22:56:18 JST 2013
+//
+
+#include "aabro.h"
+
+
+describe "abr_parse_string()"
+{
+  it "flips burgers"
+  {
+    ensure(1 == 1);
+    ensure(3 == 4);
+  }
+}
+
