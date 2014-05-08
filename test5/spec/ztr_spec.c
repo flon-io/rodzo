54c498c2691a53721bb818688dc6ec37e038f4b9
John Mettraux <jmettraux@gmail.com>
2014-05-09 06:53:31 +0900

    add spec file to test5 (pending)

---
+++ test5/spec/ztr_spec.c
@@ -0,0 +1,24 @@
+
+//
+// specifying flutil
+//
+// Thu May  8 06:29:57 JST 2014
+//
+
+#define _POSIX_C_SOURCE 200809L
+#include "flutil.h"
+
+
+context "lonely pending"
+{
+
+  before all
+  {
+  }
+
+  it "is pending"
+  //{
+  //  ensure(1 == 1);
+  //}
+}
+
